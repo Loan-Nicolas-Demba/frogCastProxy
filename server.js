d6c4d728b85deda5126804f30c9921b693cd2d3b
nicolalo1 <Loan.Nicolas-demba@etu.univ-grenoble-alpes.fr>
2026-05-04 10:40:08 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+
+
+const apiKey = process.env.FROGCAST_API_KEY;
+app.get("/forecast", async (req, res) => {
+  try {
+    const { latitude, longitude, fields } = req.query;
+
+    const apiUrl =
+      `https://api.frogcast.com/api/v1/forecast/?latitude=${encodeURIComponent(latitude)}` +
+      `&longitude=${encodeURIComponent(longitude)}` +
+      `&fields=${encodeURIComponent(fields)}`;
+
+    const response = await fetch(apiUrl, {
+      headers: {
+        Authorization: `Token ${process.env.FROGCAST_API_KEY}`
+      }
+    });
+    console.log(apiUrl);
+    const text = await response.text();
+
+    if (!response.ok) {
+      return res.status(response.status).send(text);
+    }
+
+    const data = JSON.parse(text);
+    res.json(data);
+  } catch (error) {
+    console.error("Proxy error:", error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+
+
+app.listen(PORT, () => {
+  console.log(`Proxy running on http://localhost:${PORT}`);
+});
